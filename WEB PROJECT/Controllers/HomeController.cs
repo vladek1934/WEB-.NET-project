b76ece9e0fa11ff56ecf09ea3cbf4a37a4762332
mun vlad <vlad.mun1999@gmail.com>
2017-12-06 02:19:02 +0600

    Better

--- WEB PROJECT/Controllers/HomeController.cs
+++ WEB PROJECT/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using WEB_PROJECT.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +10,59 @@ namespace WEB_PROJECT.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
         }
-
+        [Authorize]
         public ActionResult Draw()
         {
-            return View();
+            Images image = new Images();
+            return View(image);
         }
-
+        [Authorize]
         public ActionResult Showcase()
         {
-            return View();
+            Entities2 db = new Entities2();
+            var imagesList = (from img in db.Images select img).ToList();
+            return View(imagesList);
         }
+        [Authorize]
         public ActionResult Drawings()
         {
-            return View();
+            Entities2 db = new Entities2();
+            var user = db.AspNetUsers.Find(User.Identity.GetUserId());
+            var imagesList = user.Images.ToList();
+            return View(imagesList);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Draw(HttpPostedFileBase image)
+        {
+            Entities2 db = new Entities2();
+            Images model = new Images();
+            model.UserID = User.Identity.GetUserId();
+            if (image != null)
+            {
+                model.Name = User.Identity.GetUserName() + " WORK ";
+                model.Image = new byte[image.ContentLength];
+                image.InputStream.Read(model.Image, 0, image.ContentLength);
+                db.Images.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Drawings");
+            }
+            else return Index();
+        }
+        [Authorize]
+        public ActionResult Delete(int id)
+        {
+            Entities2 db = new Entities2();
+            Images img = db.Images.Find(id);
+            db.Images.Remove(img);
+            db.SaveChanges();
+            return RedirectToAction("Drawings");
         }
     }
 }
---
+++ WEB PROJECT/Scripts/paint.js
@@ -0,0 +1,48 @@
+﻿window.onload = function () {
+
+    var canvas = document.querySelector('#canvas');
+    var ctx = canvas.getContext('2d');
+
+    var sketch = document.querySelector('#paint');
+    var sketch_style = getComputedStyle(sketch);
+    canvas.width = parseInt(sketch_style.getPropertyValue('width'))*0.9;
+    canvas.height = parseInt(sketch_style.getPropertyValue('width')) * 0.4;
+
+    var mouse = { x: 0, y: 0 };
+
+    /* Mouse Capturing Work */
+    canvas.addEventListener('mousemove', function (e) {
+        mouse.x = e.pageX - this.offsetLeft;
+        mouse.y = e.pageY - this.offsetTop;
+    }, false);
+
+    /* Drawing on Paint App */
+    ctx.lineWidth = 5;
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = 'blue';
+
+    canvas.addEventListener('mousedown', function (e) {
+        ctx.beginPath();
+        ctx.moveTo(mouse.x, mouse.y);
+
+        canvas.addEventListener('mousemove', onPaint, false);
+    }, false);
+
+    canvas.addEventListener('mouseup', function () {
+        canvas.removeEventListener('mousemove', onPaint, false);
+    }, false);
+
+    var onPaint = function () {
+        ctx.lineTo(mouse.x, mouse.y);
+        ctx.stroke();
+    };
+
+    function downloadCanvas(link, canvasId, filename) {
+        link.href = document.getElementById(canvasId).toDataURL();
+        link.download = filename;
+    }
+    document.getElementById('download').addEventListener('click', function () {
+        downloadCanvas(this, 'canvas', 'test.png');
+    }, false);
+};
